8a27081944308663e75dd2a9a8c3d68144012350
Ayaz Ahmed <93036472+1Ayazahmed@users.noreply.github.com>
2026-01-13 21:24:32 +0500

    Update Firebase config to use environment variables
    Replaced hardcoded Firebase configuration with environment variables.

--- filmy/src/firebase/firebase.js
+++ filmy/src/firebase/firebase.js
@@ -12,24 +12,8 @@ import { getFirestore, collection } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAUDjT_6YotxNXMsBc3i2qF52yUCfOZuz4",
-  authDomain: "filmiziashop.firebaseapp.com",
-  projectId: "filmiziashop",
-  storageBucket: "filmiziashop.appspot.com",
-  messagingSenderId: "494606534289",
-  appId: "1:494606534289:web:b540c3dfaa17fb45a4e130",
-  measurementId: "G-DC17MTHMJQ",
 };
 
-// const firebaseConfig = {
-//   apikey: process.env.apikey,
-//   authDomain: process.env.authDomain,
-//   projectId: process.env.projectId,
-//   storageBucket: process.env.storageBucket,
-//   messagingSenderId: process.env.messagingSenderId,
-//   appId: process.env.appId,
-//   measurementId: process.env.measurementId,
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
